5205db336c839f4275d84e30edd5d6848ab9eeb6
Ribeiro <ribeiro.ajr@icloud.com>
2025-12-30 16:46:08 -0300

    changes urls2

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'agenda'
 urlpatterns = [
-    path('', views.agenda_anual, name='anual'),
+    path('anual/', views.agenda_anual, name='anual'),
     path('gerar-pdf/', views.gerar_pdf, name='gerar_pdf'),
     # path para upload/import se criar uma view web
 ]
